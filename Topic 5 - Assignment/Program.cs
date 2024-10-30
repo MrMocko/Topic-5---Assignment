8146c1820e4db9d4ca823ec991cf9dc70c5753e5
Andre Mocko <339723967@GoTVDSB.ca>
2024-10-30 09:34:54 -0400

    Done

--- Topic 5 - Assignment/Program.cs
+++ Topic 5 - Assignment/Program.cs
@@ -7,8 +7,8 @@ namespace Topic_5___Assignment
         static void Main(string[] args)
         { // Andre
             Double earthWeight, planetWeight;
-            string planet, brock, answer, princess;
-            int secret, guess, pair;
+            string planet, brock, answer, princess, multChoiceAns;
+            int secret, guess, pair, point =0;
 
 
 
@@ -80,25 +80,31 @@ namespace Topic_5___Assignment
             Int32.TryParse (Console.ReadLine(), out pair);
             if (pair == 20 )
             {
+                Console.WriteLine("");
                 Console.WriteLine("Okkkkk okkkk man good work you got it right");
+                point += 1;
             }
             else
             {
+                Console.WriteLine("");
                 Console.WriteLine("Bro are you serious?");
             }
             Console.WriteLine("");
             Console.WriteLine("Ok next question");
             Console.WriteLine("");
             Console.WriteLine("What is Obama's last name?");
+            Console.Write("Enter your answer here: ");
             brock = Console.ReadLine();
             if (brock.ToLower() == "obama")
             {
                 Console.WriteLine("Oh yea man good job :)");
+                point += 1;
             }
             else
             {
                 Console.WriteLine("HAHA YOU ARE 100% WRONG!!!");
             }
+            Console.WriteLine("");
             Console.WriteLine("Ok heres another question.");
             Console.WriteLine("");
             Console.WriteLine("Which Disney princess is known for her long, golden hair and a magical healing ability?");
@@ -115,6 +121,7 @@ namespace Topic_5___Assignment
             else if (answer.ToLower() == "b" || answer.ToLower() == "rapeunzel")
             {
                 Console.WriteLine("OMG YOU GOT IT RIGHT!");
+                point += 1;
             }
             else if (answer.ToLower() == "c" || answer.ToLower() == "snow white")
             {
@@ -124,8 +131,48 @@ namespace Topic_5___Assignment
             {
                 Console.WriteLine("100% wrong on so many levels");
             }
+            Thread.Sleep(1000);
             Console.WriteLine("Ok good job so far, heres the last question!!! GOOD LUCK!!!");
-
+            Console.WriteLine("");
+            Thread.Sleep(2000);
+            Console.WriteLine("True or Flase is water wet?");
+            Console.Write("Enter your answer here: ");
+            multChoiceAns = Console.ReadLine();
+            if (multChoiceAns.ToLower() == "f" ||  multChoiceAns.ToLower() == "false")
+            {
+                Console.WriteLine("YES GOOD JOB!!!!!");
+                point += 1;
+            }
+            
+            else
+            {
+                Console.WriteLine("You are 100% wrong on so many levels!!!");
+            }
+            Thread.Sleep(1000);
+            Console.Write("Congrats you finished the quiz here is your score: " + point);
+            Console.WriteLine(" Here is your final percentage.");
+            Thread.Sleep(1500);
+            if (point == 4)
+            {
+                Console.WriteLine("You got 100% Good job");
+            }
+            else if (point == 3)
+            {
+                Console.WriteLine("You got a 75%");
+            }
+            else if (point == 2)
+            {
+                Console.WriteLine("You got a 50% you are boarderline lucky");
+            } 
+            else if (point == 1)
+            {
+                Console.WriteLine("You got a 25% man you failed...");
+            }
+            else if (point == 0)
+            {
+                Console.WriteLine("Just leave please...");
+            }
+            Console.ReadLine();
 
 
 
